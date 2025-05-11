8bb2b09a098a59187f955cd9534765e34534ed02
MRSgkce <mursidegokce985@gmail.com>
2025-05-11 18:11:17 +0300

    Interface designed. Buttons added. Keyboard settings are being made.

--- weatherApp/ViewController.swift
+++ weatherApp/ViewController.swift
@@ -7,13 +7,32 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITextFieldDelegate {
+    @IBOutlet weak var label2: UILabel!
+    
+    @IBOutlet weak var searcharea: UITextField!
+    @IBOutlet weak var symbols: UIImageView!
+    @IBOutlet weak var labelweather: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        searcharea.delegate = self
+        
     }
 
-
+    @IBAction func searchButoon(_ sender: UIButton) {
+        searcharea.endEditing(true)
+        print(searcharea.text!)
+    }
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        searcharea.endEditing(true)
+        print(searcharea.text!)
+        return true
+        
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        searcharea.text = ""
+    }
+    
 }
 
